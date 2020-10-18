0830dbeb8f50332015d52fff5e7bc9a2e49e4dba
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-10-17 21:00:35 -0300

    Instalado bootstrap, reactstrap, criado componentes, iniciando projeto.

---
+++ web/src/components/Generos/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Generos = () => {
+	return (
+		<div>
+			<h1>Generos</h1>
+		</div>
+	)
+}
+
+export default Generos
---
+++ web/src/components/Header/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import {
+	Nav,
+	Navbar,
+	NavbarBrand,
+	NavItem,
+	NavLink,
+	Collapse,
+	NavbarToggler,
+} from 'reactstrap'
+
+const Header = () => {
+	const [open, setOpen] = React.useState(false)
+
+	return (
+		<Navbar color="light" light expand="md">
+			<NavbarBrand tag={Link} to="/">
+				Minhas Séries
+			</NavbarBrand>
+			<NavbarToggler onClick={() => setOpen(!open)} />
+			<Collapse isOpen={open} navbar>
+				<Nav className="ml-auto" navbar>
+					<NavItem>
+						<NavLink tag={Link} to="/generos">
+							Genêros
+						</NavLink>
+					</NavItem>
+				</Nav>
+			</Collapse>
+		</Navbar>
+	)
+}
+
+export default Header
